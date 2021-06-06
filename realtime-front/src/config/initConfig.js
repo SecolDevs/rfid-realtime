9a3fe3410a711206eac8270378ed299a016a5eee
castle <felipebenavides03@gmail.com>
2021-06-06 10:21:23 -0500

    +

--- realtime-front/src/config/initConfig.js
+++ realtime-front/src/config/initConfig.js
@@ -2,6 +2,6 @@ const localhost = 'http://localhost/'
 const localYordi = 'http://192.168.0.100/'
 const localC = 'http://0.0.0.0/'
 
-const url = localYordi
+const url = localhost
 
 export default url
